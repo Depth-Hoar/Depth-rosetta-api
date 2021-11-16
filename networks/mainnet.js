19736d3df59f31b84ef9423046426ff81775fb9b
Adam <44712760+Depth-Hoar@users.noreply.github.com>
2021-11-16 15:05:44 -0700

    Update mainnet.js

--- networks/mainnet.js
+++ networks/mainnet.js
@@ -3,23 +3,23 @@ const metadata = require('./metadata/mainnet-metadata.json');
 
 module.exports = {
   blockchain: 'Substrate',
-  network: 'Dock Mainnet',
+  network: 'Edgeware Mainnet',
   nodeAddress: 'ws://localhost:9944', // This expects you have a synced local node running!
-  ss58Format: 22,
+  ss58Format: 22, // may need to change
   properties: {
-    ss58Format: 22,
-    tokenDecimals: 6,
-    tokenSymbol: 'DCK',
+    ss58Format: 22, // may need to change
+    tokenDecimals: 18,
+    tokenSymbol: 'EDG',
     poaModule: {
-      treasury: '3EnzzoFZSBeDcQ36xu8GpfMw4MU5uDmUatskoAaSg1JBxQPb',
+      treasury: 'jz77v8cHXwEWbPnbfQScXnU9Qy5VkHnDLfpDsuDYUZ7ELae',
     },
   },
-  genesis: '0xf73467c6544aa68df2ee546b135f955c46b90fa627e9b5d7935f41061bb8a5a9',
-  name: 'Dock Mainnet',
-  specName: 'dock-main-runtime',
+  genesis: '0x742a2ca70c2fda6cee4f8df98d64c4c670a052d9568058982dad9d5a7a135c5b',
+  name: 'Edgeware Mainnet', // TODO change
+  specName: 'Edgeware',
   // Next 2 fields need to change whenever they change on the chain.
-  specVersion: 19,
-  transactionVersion: 1,
+  specVersion: 46,
+  transactionVersion: 1, // TODO change
   types,
   metadataRpc: metadata.metadataRpc,
 };

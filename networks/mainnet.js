82345f29fb67372124dd496108c97f46a0a72ce6
Adam <44712760+Depth-Hoar@users.noreply.github.com>
2021-11-24 13:44:38 -0700

    meta data hex rosetta config

--- networks/mainnet.js
+++ networks/mainnet.js
@@ -3,7 +3,7 @@ const metadata = require('./metadata/mainnet-metadata.json');
 
 module.exports = {
   blockchain: 'Substrate',
-  network: 'Edgeware Mainnet',
+  network: 'Edgeware',
   nodeAddress: 'ws://localhost:9944', // This expects you have a synced local node running!
   ss58Format: 22, // may need to change
   properties: {
@@ -15,7 +15,7 @@ module.exports = {
     },
   },
   genesis: '0x742a2ca70c2fda6cee4f8df98d64c4c670a052d9568058982dad9d5a7a135c5b',
-  name: 'Edgeware Mainnet', // TODO change
+  name: 'Edgeware',
   specName: 'Edgeware',
   // Next 2 fields need to change whenever they change on the chain.
   specVersion: 46,
